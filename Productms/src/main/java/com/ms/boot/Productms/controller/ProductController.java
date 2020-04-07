ee6ec4fe7178f3d0c9d931bed9b829b50c23d3d2
w2cluster01@iiht.tech <w2cluster01@iiht.tech>
2020-04-07 16:28:50 +0530

    productms + config server

---
+++ Productms/src/main/java/com/ms/boot/Productms/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.ms.boot.Productms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/product")
+public class ProductController {
+
+}
---
+++ Productms/src/main/java/com/ms/boot/Productms/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.ms.boot.Productms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+}
